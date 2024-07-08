d67efb7bfce8db0ccee1a6a8db63054955524dba
Angad Anil Gosain <angadgosain@gmail.com>
2024-07-08 19:00:25 -0400

    Developed side navbar with routing

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,27 @@
+import LeftSidebar from "@/components/left-sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-        <main>
-            <p className="text-white-1">Left Side Bar</p>
+    <div className="relative flex flex-col">
+        <main className="relative flex bg-black-3">
+            <LeftSidebar />
+
+            <section>
+              <div>
+                <div>
+                  <Image />
+                  MobileNav
+                </div>
+                <div>
+                  Toaster
+                </div>
+              </div>
+            </section>
+
             {children}
             <p className="text-white-1">Right Side Bar</p>
         </main>
